9d6b00dbdaea85498704411620617bae66f34749
Abraham Gutierrez <labraham1986@gmail.com>
2024-03-28 17:50:38 -0500

    add validations to dynamic components

--- src/app/reactive/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/dynamic-page/dynamic-page.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Form, FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dynamic-page',
@@ -11,30 +11,59 @@ export class DynamicPageComponent {
   public myForm: FormGroup = this.fb.group({
     name: ['', [Validators.required, Validators.minLength(3)]],
     favoriteGames: this.fb.array([
-      [ 'Metal Gear', Validators.required ],
-      [ 'Death Stranding', Validators.required ],
+      ['Metal Gear', Validators.required],
+      ['Death Stranding', Validators.required],
     ]),
 
   });
 
-  constructor( private fb: FormBuilder ){}
+  constructor(private fb: FormBuilder) { }
 
-  get favoriteGameControl(){
+  get favoriteGameControl() {
 
     return this.myForm.get('favoriteGames') as FormArray;
 
   }
 
-  onSubmit(): void{
-    if(this.myForm.invalid){
+  isValidField(field: string): boolean | null {
+    return this.myForm.controls[field].errors
+        && this.myForm.controls[field].touched;
+  }
+
+  isValidFieldInArray( formArray: FormArray, index: number ){
+    return formArray.controls[index].errors
+        && formArray.controls[index].touched;
+  }
+
+  getFieldError(field: string): string | null {
+
+    if (!this.myForm.controls[field]) return null;
+    const errors = this.myForm.controls[field].errors || {};
+
+    for (const key of Object.keys(errors)) {
+      switch (key) {
+        case 'required':
+          return 'Este campo es requerido.';
+
+        case 'minlength':
+          return `Mínimo ${errors['minlength'].requiredLength} caracteres.`
+      }
+    }
+    return null;
+  }
+
+  onSubmit(): void {
+    if (this.myForm.invalid) {
       this.myForm.markAllAsTouched();
       return;
     }
-
     console.log(this.myForm.value);
     this.myForm.reset();
-
-
   }
+
+  onDeleteFavorite( index: number ): void {
+    this.favoriteGameControl.removeAt(index);
+  }
+
 
 }
