2425a7653e9db7e83f6c538d04adb3294d453ddf
Abraham Gutierrez <labraham1986@gmail.com>
2024-03-27 22:28:27 -0500

    add dynamic component

--- src/app/reactive/basic-page/basic-page.component.ts
+++ src/app/reactive/basic-page/basic-page.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
+const rtx590 = {
+  name: 'RTX 5090',
+  price: 2500,
+  inStorage: 5,
+}
 
 @Component({
   selector: 'app-basic-page',
   templateUrl: './basic-page.component.html',
   styles: ``
 })
-export class BasicPageComponent {
+export class BasicPageComponent implements OnInit {
 
   // public myForm: FormGroup = new FormGroup({
   //   name: new FormControl(''),
@@ -22,11 +28,44 @@ export class BasicPageComponent {
 
   constructor( private fb: FormBuilder){}
 
+  ngOnInit(): void {
+    // así tambien se puede llenar los formularios cuando carga la página
+    this.myForm.reset( rtx590 );
+  }
+
+  isValidField( field: string): boolean | null {
+    return this.myForm.controls[field].errors
+        && this.myForm.controls[field].touched;
+  }
+
+  getFieldError( field: string ): string | null {
+
+    if ( !this.myForm.controls[field] ) return null;
+    const errors = this.myForm.controls[field].errors || {};
+
+    for (const key of Object.keys(errors) ) {
+      switch( key ) {
+        case 'required':
+          return 'Este campo es requerido.';
+
+        case 'minlength':
+          return `Mínimo ${ errors['minlength'].requiredLength } caracteres.`
+      }
+    }
+
+    return null;
+
+  }
+
   onSave(): void{
 
-    if(this.myForm.invalid) return;
+    if(this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    };
 
     console.log(this.myForm.value);
+    this.myForm.reset({ price: 0, inStorage: 0 });
 
   }
 
