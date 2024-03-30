8a1dec97e6cd7e516215a454cab29ad28fa6d06e
Abraham Gutierrez <labraham1986@gmail.com>
2024-03-30 10:05:01 -0500

    add custom validation

--- src/app/auth/register-page/register-page.component.ts
+++ src/app/auth/register-page/register-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { cantBeStrider } from '../../shared/validators/validators';
 
 @Component({
   selector: 'app-register-page',
@@ -10,7 +11,7 @@ export class RegisterPageComponent {
   public myForm: FormGroup = this.fb.group({
     name: ['', [ Validators.required] ],
     email: ['', [ Validators.required, Validators.email ]],
-    username: ['', [Validators.required] ],
+    username: ['', [Validators.required, cantBeStrider] ],
     password: ['', [Validators.required, Validators.minLength(6)] ],
     password2: ['', [Validators.required, Validators.minLength(6)] ],
   });
