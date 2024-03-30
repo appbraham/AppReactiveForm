eb4ef520808d35beca40a9beb5b75caaefe4cddf
Abraham Gutierrez <labraham1986@gmail.com>
2024-03-29 22:44:31 -0500

    Binding register form with his controls

--- src/app/auth/register-page/register-page.component.ts
+++ src/app/auth/register-page/register-page.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register-page',
   templateUrl: './register-page.component.html',
-  styles: ``
 })
 export class RegisterPageComponent {
 
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [ Validators.required] ],
+    email: ['', [ Validators.required, Validators.email ]],
+    username: ['', [Validators.required] ],
+    password: ['', [Validators.required, Validators.minLength(6)] ],
+    password2: ['', [Validators.required, Validators.minLength(6)] ],
+  });
+
+  constructor( private fb: FormBuilder ){}
+
+  isValidField(){
+    // TODO: obtener validación desde un servicio.
+  }
+
+  onSubmit(){
+    if( this.myForm.invalid){
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.myForm.value);
+
+  }
 }
